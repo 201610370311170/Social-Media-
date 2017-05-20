5c24f63abfabe4f6fd2e311c00afd3dbeb54508a
201610370311176 <yohio914@gmail.com>
2017-05-20 17:02:52 +0700

    Update Sosmed.java

--- src/sosmed/Sosmed.java
+++ src/sosmed/Sosmed.java
@@ -1,22 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sosmed;
 
-/**
- *
- * @author Rofiif
- */
-public class Sosmed {
 
-    /**
-     * @param args the command line arguments
-     */
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class Sosmed {
     public static void main(String[] args) {
         
-      String nama;
+      System.out.println("        LOGIN  ");
+        System.out.print("Username\t: ");
+        String login = input.nextLine();
+        
+        System.out.print("Password\t: ");
+        String login2 = input.nextLine();
+        
     }
     
 }
