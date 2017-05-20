5e2412343032d0c50dcb36b3a53ac92c5ac64a59
Elsandy <sandykloning@gmail.com>
2017-05-20 17:18:58 +0700

    Update Sosmed.java

--- src/sosmed/Sosmed.java
+++ src/sosmed/Sosmed.java
@@ -17,6 +17,25 @@ public class Sosmed {
         System.out.print("Password\t: ");
         String login2 = input.nextLine();
         
+        
+        if(login.equals("user") && login2.equals("user")){
+        
+        //INI GRUP 1 
+        ArrayList<data> grup1 = new ArrayList<>();
+        grup1.add(new data("Denis", "Laki-laki", "MALANG", "Denis@gmail.com"));
+        grup1.add(new data("Ropip", "Laki-laki", "SURABAYA", "Ropip@gmail.com"));
+        grup1.add(new data("Yudi", "Laki-laki", "MADURA", "Yudi@gmail.com"));
+        grup1.add(new data("Sugiono", "Laki-laki", "JAKARTA", "Sugiono@gmail.com"));
+        grup1.add(new data("Arin", "Perempuan", "BBANYUWANGI", "Arin@gmail.com"));
+                
+        ArrayList<data> grup2 = new ArrayList<>();
+        grup2.add(new data("Irhaz", "Laki-laki", "LAMPUNG", "Irhza@gmail.com"));
+        grup2.add(new data("Freya", "Perempuan", "KALIMANTAN", "Freya@gmail.com"));
+        grup2.add(new data("Valhalla", "Laki-laki", "KEDIRI", "Valhalla@gmail.com"));
+        grup2.add(new data("Rere", "Perempuan", "MOJOKERTO", "Rere@gmail.com"));
+        grup2.add(new data("Debora", "Perempuan", "MALANG", "Debora@gmail.com"));
+        
+        
     }
     
 }
