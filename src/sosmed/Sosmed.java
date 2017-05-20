dbc523144ab668f654255723daddb124f41f20e7
201610370311176 <yohio914@gmail.com>
2017-05-20 16:59:51 +0700

    Update Sosmed.java

--- src/sosmed/Sosmed.java
+++ src/sosmed/Sosmed.java
@@ -15,7 +15,8 @@ public class Sosmed {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+      String nama;
     }
     
 }
